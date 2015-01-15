041c60403025b99928d9a97b4b02f921a4f025f9
Scott <mic1028002@gmail.com>
2015-01-15 21:38:02 +0800

    remove database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_green_galaxy_session'
+Rails.application.config.session_store :cookie_store, key: '_greengalaxy_session'
